1e681d8a46ac3d62f5a8de3b50a2740c182de088
Preethi Kasireddy <iam.preethi.k@gmail.com>
2016-12-04 15:53:45 -0800

    Set up initial model and view

--- PhotoApp.elm
+++ PhotoApp.elm
@@ -3,16 +3,37 @@ module PhotoApp exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
+urlPrefix = 
+  "http://elm-in-action.com/"
+
 view model =
   div [ class "content" ]
     [ h1 [] [ text "Photo App" ]
     , div [ id "thumbnails" ]
-      [ img [ src "http://elm-in-action.com/1.jpeg" ] []
-      , img [ src "http://elm-in-action.com/2.jpeg" ] []
-      , img [ src "http://elm-in-action.com/3.jpeg" ] []
-      ]
+      (List.map (viewThumbnail model.selectedUrl) model.photos)
+    , img
+        [ class "large"
+        , src (urlPrefix ++ "large/" ++ model.selectedUrl)
+        ]
+        []
     ]
 
-main = 
-  view "No model yet"
+viewThumbnail selectedUrl thumbnail =
+  img [ src (urlPrefix ++ thumbnail.url)
+      , classList [ ("selected", selectedUrl == thumbnail.url) ]
+      ]
+      []
+
+initialModel = 
+  { photos = 
+      [ { url = "1.jpeg" }
+      , { url = "2.jpeg" }
+      , { url = "3.jpeg" }
+      ]
+  , selectedUrl = "1.jpeg" 
+  }
   
+
+main = 
+  view initialModel
+
